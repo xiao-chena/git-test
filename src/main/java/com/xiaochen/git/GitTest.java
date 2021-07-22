9e46a053e815330a37c91fe8706c7b3fbdb43096
xiaochen <381831785@qq.com>
2021-07-22 17:27:16 +0800

    push commit

--- src/main/java/com/xiaochen/git/GitTest.java
+++ src/main/java/com/xiaochen/git/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
         System.out.println("hello git4!");
         System.out.println("master test!");
         System.out.println("hot-fix test2");
+        System.out.println("push test!");
     }
 }
